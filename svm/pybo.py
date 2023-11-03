724906515c6bfa59b0823ca092d3b43ee77dcd6a
leeseulkwak <leesel1690@gmail.com>
2023-11-03 12:04:17 +0900

    231103

---
+++ svm/pybo.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import svm.test as test
+
+ # svm에 폴더안에 test파일이 있기 떄문에 import 실행시 이렇게 해야함.
+
+app = Flask(__name__)
+comparison_str=test.comparison.to_string()
+
+@app.route('/')
+def hello_pybo():
+    return comparison_str
+    # return 'Hello, Pybo22222'
---
+++ svm/test.py
@@ -0,0 +1,25 @@
+# test.py
+
+import svc_param_selection
+from sklearn.metrics import classification_report
+
+train = svc_param_selection.test
+clf = svc_param_selection.clf
+pd = svc_param_selection.pd
+
+X_test = train[['3P', 'BLK']]
+y_test = train[['Pos']]
+
+
+y_true, y_pred = y_test, clf.predict(X_test)
+
+# print(classification_report(y_true, y_pred))
+
+comparison = pd.DataFrame({'prediction': y_pred,
+                           'ground_truth': y_true.values.ravel()})
+
+# print(comparison)
+
+if __name__=="__main__":
+    print(classification_report(y_true, y_pred))
+    print(comparison)
